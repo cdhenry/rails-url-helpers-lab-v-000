b6560499f79218e10cdf87ea0b4a2cb464d3b750
Chris Henry <cdhenry@gmail.com>
2018-03-27 22:06:18 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,7 +11,7 @@ class StudentsController < ApplicationController
   def activate
     set_student
     @student.active = !@student.active
-    redirect to '/show'
+    render '/show'
   end
 
   private
